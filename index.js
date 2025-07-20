d1a4854f20348464727594f3f264ca5b6413d53c
SebClav <sebasclavijoalessandri@gmail.com>
2025-07-19 23:32:15 -0400

    Setup Apollo Server with Express and integrate database and Firebase context

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import tourismdb from './src/config/mysql.js';
+import firebase from './src/config/firebase.js';
 import dotenv  from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
+const server = new ApolloServer({
+  context: () => ({
+    db: tourismdb,
+    firebase
+  }),
+});
+
+await server.start();
+server.applyMiddleware({ app });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  
+  console.log(`http://localhost:${PORT}`);
 });
