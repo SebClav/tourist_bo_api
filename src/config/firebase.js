7fd467996f7273b1226b8a924da2888bf903ea75
SebClav <sebasclavijoalessandri@gmail.com>
2025-07-26 14:08:57 -0400

    Add Firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,2 +1,10 @@
-const firebase = {}; 
+const firebase = {
+  apiKey: "AIzaSyDfD7-GW5WjbHd2xqWVXa1ycJ6JA8YSkSc",
+  authDomain: "tourism-bo.firebaseapp.com",
+  projectId: "tourism-bo",
+  storageBucket: "tourism-bo.firebasestorage.app",
+  messagingSenderId: "800796013293",
+  appId: "1:800796013293:web:54ba51895a7e1a972712e4"
+};
+
 export default firebase;
