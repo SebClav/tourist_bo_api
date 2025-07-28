3ee17cc74285db3231bbde2cd3adad3776c70a99
SebClav <sebasclavijoalessandri@gmail.com>
2025-07-28 00:58:52 -0400

    Remove unused 'obtenerRuta' resolver from GraphQL resolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -42,36 +42,6 @@ const resolvers = {
 
       return rutasCombinadas;
     },
-
-    async obtenerRuta(_, { nombreRuta }, { firebase }) {
-      const firestore = firebase;
-      const docRef = doc(firestore, "coordenadas_rutas", "rutas");
-      const docSnap = await getDoc(docRef);
-
-      if (!docSnap.exists()) {
-        throw new Error("Documento de rutas no encontrado");
-      }
-
-      const rutas = docSnap.data();
-
-      if (!rutas[nombreRuta]) {
-        throw new Error(`Ruta "${nombreRuta}" no encontrada en la base de datos`);
-      }
-
-      const coordenadasRaw = rutas[nombreRuta];
-      
-      const coordenadas = coordenadasRaw.map((coord) => {
-            return { 
-              lat: coord.latitude, 
-              lng: coord.longitude 
-            };
-        }); //Fetching coordinates from Firebase of the 'geopoint' type.
-
-      return {
-        id: nombreRuta,
-        coordenadas
-      };
-    },
   }
 };
 
