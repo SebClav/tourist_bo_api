667e178ac66020e27b2994b405e0e7d932dc92cd
SebClav <sebasclavijoalessandri@gmail.com>
2025-07-29 16:13:58 -0400

    Refactor Firebase configuration to use environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfD7-GW5WjbHd2xqWVXa1ycJ6JA8YSkSc",
-  authDomain: "tourism-bo.firebaseapp.com",
-  projectId: "tourism-bo",
-  storageBucket: "tourism-bo.firebasestorage.app",
-  messagingSenderId: "800796013293",
-  appId: "1:800796013293:web:54ba51895a7e1a972712e4"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 const app = initializeApp(firebaseConfig);
